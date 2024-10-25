2a944acc99bb1fedc3525fbe79802d8f9c6556fb
shakeen <shakeenkumar33@gmail.com>
2024-10-25 19:16:40 +0530

    art

--- articals.js
+++ articals.js
@@ -1,17 +1,35 @@
-function renderArticles() {
-    const articlesContainer = document.querySelector("#articles");
-
-    articles.forEach(item => {
-        const article = document.createElement("article");
-        article.innerHTML = `
-            <img src="${item.imgSrc}" alt="${item.imgAlt}">
-            <h2>${item.title}</h2>
-            <p class="details">Date: ${item.date} | Ages: ${item.ages} | Genre: ${item.genre}</p>
-            <p>${item.description}</p>
-            <p class="rating">${item.stars}</p>
-        `;
-        articlesContainer.appendChild(article);
-    });
-}
-
-renderArticles();
+const articles = [
+    {
+        id: 1,
+        title: "Septimus Heap Book One: Magyk",
+        date: "July 5, 2022",
+        description: "If you enjoy stories about seventh sons of seventh sons and magyk, this is the book for you.",
+        imgSrc: "./images/magyk.jpg",
+        imgAlt: "Book cover for Septimus Heap 1",
+        ages: "10-14",
+        genre: "Fantasy",
+        stars: "⭐⭐⭐⭐"
+    },
+    {
+        id: 2,
+        title: "Magnus Chase Book One: Sword of Summer",
+        date: "December 12, 2021",
+        description: "The anticipated new novel by Rick Riordan, featuring Norse mythology.",
+        imgSrc: "./images/sword_of_summer.jpg",
+        imgAlt: "Book cover for Magnus Chase 1",
+        ages: "12-16",
+        genre: "Fantasy",
+        stars: "⭐⭐⭐⭐"
+    },
+    {
+        id: 3,
+        title: "Belgariad Book One: Pawn of Prophecy",
+        date: "Feb 12, 2022",
+        description: "A fierce dispute among the Gods leaves the World divided into five kingdoms...",
+        imgSrc: "./images/pawn_of_prophecy.jpg",
+        imgAlt: "Book cover for Pawn of Prophecy",
+        ages: "12-16",
+        genre: "Fantasy",
+        stars: "⭐⭐⭐⭐⭐"
+    }
+];
